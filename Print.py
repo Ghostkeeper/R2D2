ed9e97d901912f2fef8c8186a4c29a1c6e64b547
Cherubim <rubend@tutanota.com>
2018-07-16 22:15:43 +0200

    Remove license
    You are now no longer allowed to use this software.
    This decision hurts a little bit but it does allow me some more leverage towards Ultimaker to make sure that they eventually use this software in a free manner.
    
    Previous versions of the software can be used freely, but are pretty much useless for anyone.

--- Print.py
+++ Print.py
@@ -1,8 +1,5 @@
 #Plug-in to gather after-print feedback to tune your profiles, optimising for certain intent.
 #Copyright (C) 2018 Ghostkeeper
-#This plug-in is free software: you can distribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License or (at your option) any later version.
-#This plug-in is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
-#You should have received a copy of the GNU Affero General Public License along with this library. If not, see <https://gnu.org/licenses/>.
 
 import PyQt5.QtCore #This object's fields are accessible from QML.
 
