06717950705915af2d9e6e443a0ef894ee625108
Cherubim <rubend@tutanota.com>
2018-08-19 00:31:34 +0200

    Split data parameter up into X and Y
    These are the inputs of the formula for least squares: B = (X^T * X)^-1 * X^T * Y. X is the predictor, Y is the response, B is the resulting list of multipliers. Should be simple to implement now, since most of the work is getting the input data in the correct format.

--- LeastSquares.py
+++ LeastSquares.py
@@ -9,16 +9,19 @@ class LeastSquares:
 	polynomial, even though the approximation algorithm is linear.
 	"""
 
-	def train(self, data, highest_exponent = 4):
+	@staticmethod
+	def train(predictors, responses, highest_exponent = 4):
 		"""
 		Fit a polynomial to the specified data.
 
 		This data can have any number of dimensions, but once the number of
 		dimensions times the highest exponent starts to exceed around ~2000,
 		this training method is not very efficient any more.
-		:param data: A two-dimensional table. In the major coordinates are
-		each dimension of the data (each field). In the minor coordinates are
-		the individual measurements.
+		:param predictors: A two-dimensional table of the predictors. In the
+		major coordinates are the individual measurements. In the minor
+		coordinates are the dimensions of data, each field of the measurement.
+		:param responses: A one-dimensional list of the responses for each of
+		the measurements.
 		:return: A list containing the multipliers of each exponent.
 		"""
 		#TODO.
